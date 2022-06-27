b60456a2dc3b1ea414058a12bf963c785efe8375
박세준 <tpwns6693@gmail.com>
2022-06-27 16:29:25 +0900

    테스트용 파일 작성

---
+++ TDD_tutorial/tdd2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import OrderPage from './pages/orderPage/OrderPage';
+
+function App() {
+  return (
+    <div style={{padding: "4rem"}}>
+      <OrderPage />
+    </div>
+  );
+}
+
+export default App;
